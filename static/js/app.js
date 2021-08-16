ccf337fd599a1e544dd32e06101f404595cb6094
Jared Behler <jediracer13@gmail.com>
2021-08-15 21:49:36 -0400

    add app.js, data.js, index.html and style.css

---
+++ static/js/app.js
@@ -0,0 +1,14 @@
+// import the data from data.js
+const tableData = data;
+
+//reference the HTML table using 3d
+var tbody = 3d.select("tbody");
+
+//simple js console.log statement
+function printHello(){
+    console.log("Hello there!");
+};
+
+function buildTable(data) {
+    tbody.html("");
+}
